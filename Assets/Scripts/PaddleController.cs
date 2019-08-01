76c07801847cd0e4658546f1b228fa535742edb4
wahyuokay9 <wakay.98@gmail.com>
2019-08-01 15:52:52 +0700

    1. Make paddle 2. Insert input method 3. Create folder Assets and scripts PaddleController 4. Insert motion and limits for paddle in PaddleController.cs

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float speed; //Speed motion
+    public string axis; //Setting keyboard input
+    public float topEdge;
+    public float bottomEdge;
+    void Start()    // Start is called before the first frame update
+    {
+        
+    }
+
+    void Update()   // Update is called once per frame
+    {
+        float motion = Input.GetAxis(axis) * speed * Time.deltaTime;    //Get the paddle movement
+        float nextPos = transform.position.y + motion; //Predict the next position
+        if (nextPos > topEdge) {
+            motion = 0;
+        }
+        if (nextPos < bottomEdge) {
+            motion = 0;
+        }
+        transform.Translate(0, motion, 0);  //Implementation to Paddle
+    }
+}
